ff09e40fb9ce36e9b63c3b153e151ae35d633072
Dheeraj Singh <dheeraj@panalinks.com>
2016-07-06 10:26:18 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,181 @@
+<html>
+<body>
+
+<!--Top Drop Down Menu 1 HTML-->
+<ul id="ddsubmenu1" class="ddsubmenustyle">
+<li><a href="collection_homeaccessories.html">HOME ACCESSORIES</a>
+                <ul>
+                  <li><a href="collection_homeaccessories_ditems.html">DECORATIVE 
+                    ITEMS</a> 
+                    <ul>
+                      <li><a href="collection_homeaccessories_ditems_accentpieces.html">ACCENT 
+                        PIECES</a></li>
+                      <li><a href="collection_homeaccessories_ditems_boxes.html">BOXES</a></li>
+                      <li><a href="collection_homeaccessories_ditems_candle.html">CANDLE 
+                        HOLDERS</a></li>
+                      <li><a href="collection_homeaccessories_ditems_boxes_candlesnew.html">CANDLE</a></li>
+                      <li><a href="collection_homeaccessories_ditems_dplates.html">DECORATIVE 
+                        PLATES</a></li>
+                      <li><a href="collection_homeaccessories_ditems_pframes.html">PHOTO 
+                        FRAMES</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="collection_homeaccessories_vna.html">VASES, ASHTRAYS 
+                    & PLANTERS</a> 
+                    <ul>
+                      <li><a href="collection_homeaccessories_vna_vases.html">VASES</a></li>
+                      <li><a href="collection_homeaccessories_vna_ashtrays.html">ASHTRAYS</a></li>
+                      <li><a href="collection_homeaccessories_vna_planters.html">PLANTERS</a></li>
+                    </ul>
+                  </li>
+                </ul>
+</li>
+<li><a href="collection_tableware.html">TABLEWARE</a>
+                <ul>
+                  <li><a href="collection_tableware_crockery.html ">CROCKERY</a> 
+                    <ul>
+                      <li><a href="collection_tableware_crockery_dinner.html">DINNER</a></li>
+                      <li><a href="collection_tableware_crockery_tea.html">TEA</a></li>
+                      <li><a href="collection_tableware_crockery_serving.html">SERVING</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="collection_tableware_cutlery.html">CUTLERY</a></li>
+                  <li><a href="collection_tableware_servers.html">SERVERS</a></li>
+                  <li><a href="collection_tableware_glassware.html">GLASSWARE</a> 
+                    <ul>
+                      <li><a href="collection_tableware_champagne_flutes.html">CHAMPAGNE 
+                        FLUTES</a></li>
+                      <li><a href="collection_tableware_glassware_decanters.html">DECANTERS</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="collection_tableware_other.html">OTHER ITEMS</a> 
+                    <ul>
+                      <li><a href="collection_tableware_other_barware.html">BARWARE</a></li>
+                      <li><a href="collection_tableware_other_napkin.html">NAPKIN 
+                        RINGS</a></li>
+                      <li><a href="collection_tableware_other_tablemats.html">TABLE 
+                        MATS</a></li>
+                      <li><a href="collection_tableware_other_trays.html">TRAYS</a></li>
+                    </ul>
+                  </li>
+                </ul>
+</li>
+<li><a href="collection_lighting.html">LIGHTING</a>
+                <ul>
+                  <li><a href="collection_lighting_chandlier.html">CHANDELIERS</a></li>
+                  <li><a href="collection_lighting_floorlamps.html">FLOOR LAMPS</a></li>
+                  <li><a href="collection_lighting_tablelamps.html">TABLE LAMPS</a></li>
+                  <li><a href="collection_lighting_wall.html">WALL LIGHTS</a></li>
+                </ul>
+</li>
+<li><a href="collection_cushions.html">CUSHIONS & THROWS</a></li>
+<li><a href="collection_mirror.html">MIRRORS</a></li>
+<li><a href="collection_art.html">ART</a></li>
+</ul>
+
+<!--Top Drop Down Menu 2 HTML-->
+<ul id="ddsubmenu2" class="ddsubmenustyle">
+<li><a href="furniture_livingroom.html">LIVING ROOM</a>
+                <ul>
+                  <li><a href="furniture_livingroom_sofas.html">SOFAS</a></li>
+                  <li><a href="furniture_livingroom_chairs.html">CHAIRS</a></li>
+                  <li><a href="furniture_livingroom_tables.html">TABLES</a></li>
+                  <li><a href="furniture_livingroom_consoles.html">CONSOLES</a></li>
+                  <li><a href="furniture_livingroom_ottomans.html">OTTOMANS</a></li>
+                  <li><a href="furniture_livingroom_bench.html">BENCH</a></li>
+                  <li><a href="furniture_livingroom_nestedtable.html">NESTED TABLE</a></li>
+                  <li><a href="furniture_livingroom_cabinet.html">CABINETS</a></li>
+                </ul>
+</li>
+<li><a href="furniture_diningroom.html">DINING ROOM</a>
+                <ul>
+                  <li><a href="furniture_diningroom_cabinets.html">CABINETS</a></li>
+                </ul>
+</li>
+<li><a href="furniture_bedroom.html">BEDROOM</a></li>
+<li><a href="furniture_occasional.html">OCCASIONAL</a></li>
+<li><a href="bespoke.html">BESPOKE</a></li>
+</ul>
+
+<!--Dropdown menu Ends-->
+
+		</td>
+      </tr>
+    </table>	</td>
+  </tr>
+  <tr>
+    <td><table width="1000" border="0" align="center" cellpadding="0" cellspacing="0">
+      <tr>
+        <td><iframe name="Frame1" src="slider.html" style="width: 1000px; height: 442px;" marginwidth="0" marginheight="0" vspace="0" hspace="0" frameborder="0" scrolling="no">
+</iframe></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td class="bodybg"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td><table width="993" border="0" align="center" cellpadding="0" cellspacing="0">
+          <tr>
+            <td class="hpad1"><img src="images/welcome.png" alt="Welcome to Andyy Home" width="419" height="35"></td>
+          </tr>
+          <tr>
+            <td class="bodytxt">Welcome to Andyy Home<sup class="regg">&#174;</sup> and our collection of contemporary classic and elegant home accessories and bespoke furniture. Our aim is to provide our customers with luxurious living and high quality products at realistic prices.
+			<br>
+<br>
+We stock only the best and our merchandise represents leading international brands such as Griffe of Italy for opulent glassware, Gien of France for elegant tableware, Annick Cormery of France for handmade accessories, Mis en Demure of France for gorgeous table lamps, chandeliers &amp; mirrors,  Belgian tapestry cushions & throws from The Netherlands by the famous artist Thierry Poncelet and bespoke Murano home accessories to name but a few.
+<br>
+<br>
+So please feel free to browse a selection of our range on offer, or pop down to our store in Lado Sarai, Mehrauli. If you are out of town we can arrange for the goods to be delivered. Simply click on the 'Enquire Now' button or contact us by phone or email.
+			<br>
+<br>
+<table width="993" border="0" cellspacing="0" cellpadding="0" align="center">
+  <tr>
+    <td><a class="h1" href="collection_homeaccessories.html"></a></td>
+    <td><a class="h2" href="furniture.html"></a></td>
+    <td><a class="h3" href="interiors.html"></a></td>
+  </tr>
+  <tr>
+    <td><a class="h4" href="collection_tableware.html"></a></td>
+    <td><a class="h5" href="furniture_livingroom_chairs.html"></a></td>
+    <td><a class="h6" href="collection_lighting.html"></a></td>
+  </tr>
+</table></td>
+          </tr>
+        </table></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td class="btmbg">
+	<table width="1000" border="0" cellspacing="0" cellpadding="0" align="center">
+  <tr>
+    <td>
+	<table width="1000" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td class="btm1"><table width="447" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td class="ql"><img src="images/quicklinks.png" alt="Quick Links" width="83" height="13" title="Quick Links"></td>
+        <td>&nbsp;</td>
+        </tr>
+      <tr>
+        <td class="btm2">
+		<ul id="bbullet" style="padding:0; margin:0;">
+		<li><a href="press.html">Press</a></li>
+		<li><a href="shipping.html">Shipping & Delivery</a></li>
+		<li><a href="privacy.html">Security & Privacy</a></li>
+		</ul>		</td>
+        <td>
+		<ul id="bbullet" style="padding:0; margin:0;">
+		<li><a href="terms.html">Terms & Conditions</a></li>
+		<li><a href="sitemap.html">Sitemap</a></li>
+		<li><a href="contact.html">Contact Us</a></li>
+		</ul>		</td>
+        </tr>
+    </table></td>
+    <td class="btm3"><img src="images/bstrp.gif" width="1" height="128"></td>
+    <td class="btm1"><table width="320" border="0" cellspacing="0" cellpadding="0" align="left">
+      <tr>
+        <td class="ql"><a class="thickbox" title="Andyy Home" href="map.html?height=420&amp;width=750" alt="Locate Us" ><img src="images/locatus.png" alt="Locate Us" width="65" height="12" border="0"></a></td>
+      </tr>
+    </body>
+</html>
